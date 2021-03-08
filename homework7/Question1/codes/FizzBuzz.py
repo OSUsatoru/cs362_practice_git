456f727dde30c2ddade102bb125a3f712acd1883
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-07 20:13:43 -0800

    Implemented Buzz method, Buzz output

--- homework7/Question1/codes/FizzBuzz.py
+++ homework7/Question1/codes/FizzBuzz.py
@@ -3,6 +3,8 @@ class FizzBuzz_class:
         for i in range(1,101):
             if i%3==0:
                 print("Fizz", end="")
+            elif i%5==0:
+                print("Buzz", end='')
             else:
                 print(i,end="")
             if i!=100:
