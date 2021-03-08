114c8b341a27d508a2ebdbaefcd3895ef61c2158
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-07 21:00:04 -0800

    Wrote third test, to detect input is not divisible by 100

--- homework7/Question2/tests/test_Leap_year.py
+++ homework7/Question2/tests/test_Leap_year.py
@@ -14,8 +14,10 @@ class Leap_year(unittest.TestCase):
         self.result2 = "Invalid input (sign error)"
         self.argument3 = 80
         self.result3 = str(self.argument3) + " is a leap year\n"
+        self.argument4 = 400
+        self.result4 = str(self.argument4) + " is not a leap year\n"
 
-    def test_FizzBuzz(self):
+    def test_Leap_year(self):
         with self.assertRaises(ValueError, msg = "Invalid input (type error)"):
             Leap_year_class(self.argument1).Leap_year()
         self.assertEqual(self.result2, Leap_year_class(self.argument2).Leap_year())
@@ -26,6 +28,11 @@ class Leap_year(unittest.TestCase):
         sys.stdout = sys.__stdout__
         self.assertEqual(self.result3, capturedOutput.getvalue())
 
+        capturedOutput2 = io.StringIO()
+        sys.stdout = capturedOutput2
+        Leap_year_class(self.argument4).Leap_year()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(self.result4, capturedOutput2.getvalue())
 
 if __name__ == "__main__":
     unittest.main()
