ffa68ba9d72e602403d7e53b46f852eb802d44d3
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-07 20:52:53 -0800

    Wrote second test, input is divisible by 4

--- homework7/Question2/tests/test_Leap_year.py
+++ homework7/Question2/tests/test_Leap_year.py
@@ -13,13 +13,18 @@ class Leap_year(unittest.TestCase):
         self.argument2 = -5
         self.result2 = "Invalid input (sign error)"
         self.argument3 = 80
-        self.result3 = str(self.argument1) + "is a leap year"
+        self.result3 = str(self.argument3) + " is a leap year\n"
 
     def test_FizzBuzz(self):
         with self.assertRaises(ValueError, msg = "Invalid input (type error)"):
             Leap_year_class(self.argument1).Leap_year()
         self.assertEqual(self.result2, Leap_year_class(self.argument2).Leap_year())
 
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        Leap_year_class(self.argument3).Leap_year()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(self.result3, capturedOutput.getvalue())
 
 
 if __name__ == "__main__":
