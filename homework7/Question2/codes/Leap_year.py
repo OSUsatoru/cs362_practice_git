35c478a7a0a7673d3f8a2586a657b1dd914e40c7
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-07 20:48:58 -0800

    Implemented To detect invalid method, to detect invalid input

--- homework7/Question2/codes/Leap_year.py
+++ homework7/Question2/codes/Leap_year.py
@@ -2,4 +2,9 @@ class Leap_year_class:
     def __init__(self, n):
         self.n = n
     def Leap_year(self):
-        pass
+        try:
+            int(str(self.n))
+        except ValueError:
+            raise ValueError("Invalid input (type error)")
+        if self.n < 0:
+            return "Invalid input (sign error)"
