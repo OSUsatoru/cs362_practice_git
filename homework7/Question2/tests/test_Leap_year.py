fe36c01bc3e5a6d4de39f3e7e08253dedf4bf05a
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-07 21:04:28 -0800

    Wrote fourth test, to detect leap year that is divisible by 400

--- homework7/Question2/tests/test_Leap_year.py
+++ homework7/Question2/tests/test_Leap_year.py
@@ -15,24 +15,33 @@ class Leap_year(unittest.TestCase):
         self.argument3 = 80
         self.result3 = str(self.argument3) + " is a leap year\n"
         self.argument4 = 400
-        self.result4 = str(self.argument4) + " is not a leap year\n"
+        self.result4 = str(self.argument4) + " is a leap year\n"
+        self.argument5 = 2000
+        self.result5 = str(self.argument5) + " is a leap year\n"
 
-    def test_Leap_year(self):
+    def test_FizzBuzz(self):
         with self.assertRaises(ValueError, msg = "Invalid input (type error)"):
             Leap_year_class(self.argument1).Leap_year()
         self.assertEqual(self.result2, Leap_year_class(self.argument2).Leap_year())
 
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
+        capturedOutput1 = io.StringIO()
+        sys.stdout = capturedOutput1
         Leap_year_class(self.argument3).Leap_year()
         sys.stdout = sys.__stdout__
-        self.assertEqual(self.result3, capturedOutput.getvalue())
+        self.assertEqual(self.result3, capturedOutput1.getvalue())
 
         capturedOutput2 = io.StringIO()
         sys.stdout = capturedOutput2
         Leap_year_class(self.argument4).Leap_year()
         sys.stdout = sys.__stdout__
         self.assertEqual(self.result4, capturedOutput2.getvalue())
+
+        capturedOutput3 = io.StringIO()
+        sys.stdout = capturedOutput3
+        Leap_year_class(self.argument5).Leap_year()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(self.result5, capturedOutput3.getvalue())
+
 
 if __name__ == "__main__":
     unittest.main()
